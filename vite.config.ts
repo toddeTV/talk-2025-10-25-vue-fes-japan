5ef802c099a43346ae5cdf774c932db63dd1bf4d
Thorsten Seyschab <business@todde.tv>
2025-10-24 20:54:04 +0900

    feat: use env URL var in vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,36 @@
+import type { UserConfig } from 'vite'
 import { templateCompilerOptions } from '@tresjs/core'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  plugins: [
-  ],
-  server: {
-    proxy: {
+export default defineConfig(({ mode }): UserConfig => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const proxyTarget = env.VITE_STAGE_FLOW_TOOLS_DEMO_URL
+
+  const server: UserConfig['server'] = {}
+
+  if (proxyTarget) {
+    server.proxy = {
       '/api-proxy': {
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api-proxy/, ''),
-        target: 'https://stage-flow-tools.todde.tv',
+        rewrite: (path: string) => path.replace(/^\/api-proxy/, ''),
+        target: proxyTarget,
+      },
+    }
+  }
+
+  return {
+    plugins: [
+    ],
+    server,
+    slidev: {
+      // markdown: {
+      //   markdownItSetup(md) {
+      //   },
+      // },
+      vue: {
+        ...templateCompilerOptions,
       },
     },
-  },
-  slidev: {
-    // markdown: {
-    //   markdownItSetup(md) {
-    //   },
-    // },
-    vue: {
-      ...templateCompilerOptions,
-    },
-  },
+  }
 })
