1feb3d99ac50c228f5a346c678e878f24ee3cd08
Thorsten Seyschab <business@todde.tv>
2025-10-25 01:32:35 +0900

    feat: add example SVG Wrapper

---
+++ components/SvgWrapper/Example.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// import { useNav } from '@slidev/client'
+// import { computed } from 'vue'
+// import SvgElementFilter from '../SvgElementFilter.vue'
+// import mySvgContent from '/assets/example.svg?raw'
+
+// const { clicks } = useNav()
+
+// const elementsToShow = computed<number[] | 'all'>(() => {
+//   if (clicks.value >= 9) {
+//     return 'all'
+//   }
+
+//   let res: number[] = []
+//   if (clicks.value >= 1) {
+//     res = [...res, 0, 1, 2, 12]
+//   }
+//   if (clicks.value >= 2) {
+//     res = [...res, 0, 1, 2, 3, 4, 7, 14, 16]
+//   }
+//   return res
+// })
+</script>
+
+<template>
+  foo
+  <!-- <SvgElementFilter
+    :display-indices="elementsToShow"
+    svg-class="w-full"
+    :svg-content="mySvgContent"
+  /> -->
+</template>
