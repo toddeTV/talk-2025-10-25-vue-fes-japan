fb209df187ba740b5663384e8c06245a8c336c8f
Thorsten Seyschab <business@todde.tv>
2025-10-24 19:42:37 +0900

    feat: add emoji background with QRCode to stage-flow-tools website

---
+++ components/EmojiBackground.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useStageFlowTools } from '../composables/useStageFlowTools'
+
+const { url: stageFlowToolsDemoUrl } = useStageFlowTools()
+</script>
+
+<template>
+  <div
+    v-if="stageFlowToolsDemoUrl"
+    class="w-screen h-full -z-100 absolute top-0 left-0"
+  >
+    <iframe
+      height="100%"
+      :src="stageFlowToolsDemoUrl"
+      width="100%"
+    />
+  </div>
+</template>
